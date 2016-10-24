1a54ba494ee6ef28c12f28e4167f6520229906cb
Thanh Le <lenguyenthanh@hotmail.com>
2016-10-24 20:01:11 +0700

    Apply server api follow Walkthrough video

--- server.js
+++ server.js
@@ -1,103 +1,105 @@
-#!/usr/bin/env babel-node
-
-require('./helper')
-
+const express = require('express')
+const morgan = require('morgan')
+const nodeify = require('bluebird-nodeify')
+const fs = require('fs')
 const path = require('path')
-const fs = require('fs').promise
-const Hapi = require('hapi')
-const asyncHandlerPlugin = require('hapi-async-handler')
+const mime = require('mime-types')
+const rimraf = require('rimraf')
+const mkdirp = require('mkdirp')
 
-// const cat = require('./cat')
-// const rm = require('./rm')
-// const mkdir = require('./mkdir')
-// const touch = require('./touch')
+require('songbird')
 
-function getLocalFilePathFromRequest(request) {
-  return path.join(__dirname, 'files', request.params.file)
-}
+const NODE_ENV = process.env.NODE_ENV || 'development'
+const PORT = process.env.PORT || 8000
+const ROOT_DIR = path.resolve(process.cwd()) 
+const app = express() 
+NODE_ENV === 'development' && app.use(morgan('dev'))
 
-async function readHandler(request, reply) {
-  const filePath = getLocalFilePathFromRequest(request)
+app.listen(PORT, () => console.log(`Listening @ http://127.0.0.1:${PORT}`))
 
-  console.log(`Reading ${filePath}`)
-  const data = await cat(filePath)
-  reply(data)
-}
+app.get('*', setFileMeta, sendHeaders, (req, res) => {
+  if(res.body) {
+    res.json(res.body)
+    return
+  }
+  fs.createReadStream(req.filePath).pipe(res)  
+}) 
 
-async function createHandler(request, reply) {
-  /* eslint no-unused-expressions: 0 */
-  const filePath = getLocalFilePathFromRequest(request)
+app.head('*', setFileMeta, sendHeaders, (req, res, next) => {
 
-  console.log(`Creating ${filePath}`)
+})
 
-  const stat = await fs.stat(filePath)
-  await stat.isDirectory() ? mkdir(filePath) : touch(filePath)
-  reply()
-}
-
-async function updateHandler(request, reply) {
-  const filePath = getLocalFilePathFromRequest(request)
-
-  console.log(`Updating ${filePath}`)
-  await fs.writeFile(filePath, request.payload)
-  reply()
-}
-
-async function deleteHandler(request, reply) {
-  const filePath = getLocalFilePathFromRequest(request)
-
-  console.log(`Deleting ${filePath}`)
-  await rm(filePath)
-  reply()
-}
-
-async function main() {
-  const port = 8000
-  const server = new Hapi.Server({
-    debug: {
-      request: ['error']
+app.delete('*', setFileMeta, (req, res, next) => {
+  (async () => {
+    if(!req.stat) {
+      return res.status(400).send('Invalid Path')
+    } else if(req.stat.isDirectory()) {
+      await rimraf.promise(req.filePath)      
+    } else {
+      await fs.promise.unlink(req.filePath)
     }
-  })
-  server.register(asyncHandlerPlugin)
-  server.connection({ port })
+    res.end()
+  })().catch(next)
+})
 
-  server.route([
-    // READ
-    {
-      method: 'GET',
-      path: '/{file*}',
-      handler: {
-        async: readHandler
-      }
-    },
-    // CREATE
-    {
-      method: 'PUT',
-      path: '/{file*}',
-      handler: {
-        async: createHandler
-      }
-    },
-    // UPDATE
-    {
-      method: 'POST',
-      path: '/{file*}',
-      handler: {
-        async: updateHandler
-      }
-    },
-    // DELETE
-    {
-      method: 'DELETE',
-      path: '/{file*}',
-      handler: {
-        async: deleteHandler
-      }
+
+app.put('*', setFileMeta, setDirDetails, (req, res, next) => {
+  (async () => {
+    if(req.stat) return res.status(405).send('File exists')
+
+    await mkdirp.promise(req.dirPath)  
+    
+    if (!req.isDir) {
+      req.pipe(fs.createWriteStream(req.filePath))
     }
-  ])
+    res.end()
+  })().catch(next) 
+})
 
-  await server.start()
-  console.log(`LISTENING @ http://127.0.0.1:${port}`)
+app.post('*', setFileMeta, setDirDetails, (req, res, next) => {
+  (async () => {
+    if(!req.stat) return res.status(405).send('File does not exist')
+    if(req.isDir) return res.status(405).send('Path is a directory')
+    
+    fs.promise.truncate(req.filePath, 0)
+    req.pipe(fs.createWriteStream(req.filePath))
+    res.end()
+  })().catch(next) 
+})
+
+function setDirDetails(req, res, next) {
+  const filePath = req.filePath
+  const endWithSlash = filePath.charAt(filePath.length -1) === path.sep
+  const hasExt = path.extname(filePath) !== ''
+  req.isDir = endWithSlash || !hasExt
+  req.dirPath = req.isDir ? filePath : path.dirname(filePath)
+  next()
+}
+function setFileMeta(req, res, next) {
+  const filePath = path.resolve(path.join(ROOT_DIR, req.url))
+  req.filePath = filePath
+  if(filePath.indexOf(ROOT_DIR) !== 0) {
+    res.send(400, 'Invalid path')
+    return
+  } 
+  fs.promise.stat(filePath) 
+    .then(stat => req.stat = stat, () => req.stat = null)
+    .nodeify(next)
 }
 
-main()
+function sendHeaders(req, res, next) {
+  nodeify((async () => {
+    const filePath = req.filePath
+    if (req.stat.isDirectory()) { 
+      const files = await fs.promise.readdir(filePath) 
+      res.body = JSON.stringify(files.length) 
+      res.setHeader('Content-Length', res.body.length) 
+      res.setHeader('Content-Type', 'application/json') 
+      return 
+    } 
+
+    res.setHeader('Content-Length', req.stat.size) 
+    const contentType = mime.contentType(path.extname(filePath))
+    res.setHeader('Content-Type', contentType) 
+  }) (), next)
+}
